73313e035ef90dba4a4a84f9f2f33feff68d231d
Twofyw Yang <ywywywx@gmail.com>
2019-01-26 00:12:26 +0800

    noise removed

--- preprocessing.py
+++ preprocessing.py
@@ -10,6 +10,28 @@ class DataFormatter:
     def remove_noise(self):
         self.df_raw[self.df_raw['推进速度'] > 300] = 0
 
+    def remove_anomaly(self, min_num_zeros = 5):
+        anomaly = self.df_raw['推进速度']
+        zeros = (anomaly == 0).astype('int')
+        boundary = zeros.diff()
+
+        # handle boundary condition (no pun intended)
+        if zeros.iloc[0] == 1: boundary.iloc[0] = 1
+        if zeros.iloc[-1] == 1: boundary.iloc[-1] = -1
+
+        begins = np.argwhere(np.array((boundary == 1))).squeeze()
+        ends = np.argwhere(np.array((boundary == -1))).squeeze()
+        num_zeros = [e - b for b, e in zip(begins, ends)]
+
+        # interpolate on continuous columns
+        for b, e, l in zip(begins, ends, num_zeros):
+            if l <= min_num_zeros:
+                self.df_raw.iloc[b:e,2:] = np.tile(((self.df_raw.iloc[b-1,2:] + self.df_raw.iloc[e,2:])/2).values, (l, 1))
+                print(self.df_raw['推进速度'].iloc[b:e])
+
+                
+
+
     def split_cycles1(self):
         pass
     
